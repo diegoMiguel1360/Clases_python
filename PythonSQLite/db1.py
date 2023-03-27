63465bac0d92f685d2d19dd70c9ea892045f9b62
diegoMiguel1360 <fmartinez803@misena.edu.co>
2023-03-27 14:34:37 -0500

    Python y SQLite

---
+++ PythonSQLite/db1.py
@@ -0,0 +1,14 @@
+import sqlite3 #importa modulo sqlite
+
+with sqlite3.connect('sqlitepython/conpython.db')as con:#con el metodo with creamos un flujo al archivo db
+    micursor=con.cursor()#instanciamos un objeto cursor usando el flujo creado
+    sentencia="SELECT id,nombre,apellido FROM alumno WHERE id>=400;"#la sentencia es el codigo SQL
+    #print(micursor.execute(sentencia).fetchall())
+
+def miselect(conexion,tabla,campo,operador,dato):#definimos la funcion miselect con 5 parametros
+    micursor=conexion.cursor()#instanciamos un objeto cursor usando el flujo creado en la linea 3
+    sentencia=f"SELECT * FROM {tabla} WHERE {campo}{operador}'{dato}'"#usamos la plantilla literal para definir la sentencia usando los parametros de la funcion
+    #print(sentencia)
+    print(micursor.execute(sentencia).fetchall())#se ejecuta la sentencia con el metodo .execute() y traemos todos los campos con .fetchall()
+
+miselect(con,'alumno','email','=','dbrabon2@irs.gov')#se llama la funcion con los parametros necesarios
